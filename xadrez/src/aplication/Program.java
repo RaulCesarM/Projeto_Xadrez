3859707f6b3f646f0a1ab983cd93d9f14998a600
Raul Cesar Mulerschat <raulzito737@gmail.com>
2021-06-19 23:15:23 -0300

    class position

---
+++ xadrez/src/aplication/Program.java
@@ -0,0 +1,17 @@
+package aplication;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		
+		
+		Position pos = new Position(3,2);
+		
+		System.out.println(pos);
+
+	}
+
+}
