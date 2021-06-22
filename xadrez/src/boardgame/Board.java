9a87fc3115d32a4a3ace68d1e68097e5638b1ea0
Raul Cesar Mulerschat <raulzito737@gmail.com>
2021-06-21 21:10:59 -0300

    exceptions

--- xadrez/src/boardgame/Board.java
+++ xadrez/src/boardgame/Board.java
@@ -7,6 +7,11 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+
+			throw new BoardException("erro ao criar o tabuleio");
+
+		}
 
 		this.rows = rows;
 		this.columns = columns;
@@ -18,49 +23,58 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int column) {
-		
+
+		if (!positionExists(row, column)) {
+			throw new BoardException("posição não pertence ao tabuleiro");
+		}
+
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
-		
+
+		if (!positionExists(position)) {
+			throw new BoardException("posição não pertence ao tabuleiro");
+		}
+
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
-	
 	public void placePiece(Piece piece, Position position) {
-		
-		
-		pieces[position.getRow()][position.getColumn()]=piece;
-		
-		piece.position=position;
+
+		if (thereIsAPiece(position)) {
+			throw new BoardException("ja existe uma peça nesta posição" + position);
+		}
+
+		pieces[position.getRow()][position.getColumn()] = piece;
+
+		piece.position = position;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private boolean positionExists(int row, int column) {
+
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+
+	}
+
+	public boolean positionExists(Position position) {
+
+		return positionExists(position.getRow(), position.getColumn());
+
+	}
+
+	public boolean thereIsAPiece(Position position) {
+
+		if (!positionExists(position)) {
+			throw new BoardException("posição não pertence ao tabuleiro"+position);
+		}
+
+		return piece(position) != null;
+	}
+
 }
